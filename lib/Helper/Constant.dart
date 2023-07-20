0f2814bb1e1e3f66bf5e375ac21f4b69b0aebae5
AjayMalviya <ajay.malpahwizz@gmail.com>
2023-07-20 13:23:13 +0100

    Initial commit

---
+++ lib/Helper/Constant.dart
@@ -0,0 +1,20 @@
+final String appName = 'Swipe Cart';
+
+final String packageName = 'swipecart.user';
+final String androidLink = 'https://play.google.com/store/apps/details?id=';
+
+final String iosPackage = 'swipecart.user';
+final String iosLink = 'your ios link here';
+final String appStoreId = 'swipecart.user';
+
+final String deepLinkUrlPrefix = 'https://alpha.ecommerce.link';
+final String deepLinkName = 'alpha.ecommerce.link';
+
+final int timeOut = 50;
+const int perPage = 10;
+
+// final String baseUrl = 'https://zuqzuq.com/app/v1/api/';
+final String baseUrl = 'https://developmentalphawizz.com/M-mart/app/v1/api/';
+final String imageUrl = 'https://developmentalphawizz.com/';
+
+final String jwtKey = "e1e421dceee9ea718ab764ea4fdef95629e7f415";
